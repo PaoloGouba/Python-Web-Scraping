3473ba12e26b348055677ac3ebcbf991d1c69810
paologouba <pfgouba@gmail.com>
2022-04-19 09:22:30 +0200

    update

--- Scrapy/books/books/spiders/bookstoscrap.py
+++ Scrapy/books/books/spiders/bookstoscrap.py
@@ -1,4 +1,15 @@
+from unittest import result
 import scrapy
+
+# what is yeld?
+
+# exectution in command line, scrapy runspider spider_name.py 
+
+
+
+# xpath selector
+# ciblage --> //element/child element
+# ex : //h1
 
 
 class BookstoscrapSpider(scrapy.Spider):
@@ -7,4 +18,32 @@ class BookstoscrapSpider(scrapy.Spider):
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        pass
+        
+        result = {
+            # simple xpath selector
+            #'balise_h1' : response.xpath('//h1'),
+            
+            # get text 
+            #'title' : response.xpath('//head/title/text()').get()
+            
+            #'links' : response.xpath('//a/text()').getall()
+            
+            
+            # attributes selectors
+            
+            #'alert' : response.xpath('//div[@role="alert"]/text()').get()
+            
+            
+            # get more elements
+            #'product_prices' : response.xpath('//p[@class="price_color"]/text()').getall()
+            
+            
+            #get bloc informations
+            
+            
+            
+            
+        }
+        
+        
+        yield result
